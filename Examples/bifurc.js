7971fac181bf061f336e56c574d6416a03f1d6e5
Shobi Stassen <35950152+ShobiStassen@users.noreply.github.com>
2021-04-21 11:24:51 +0800

    Update bifurc.js

--- Examples/bifurc.js
+++ Examples/bifurc.js
@@ -1,48 +1,178 @@
-var gData = {
-    "nodes": [
-        {
-          "id": "id1",
-          "name": "name1",
-          "val": 1
-        },
-        {
-          "id": "id2",
-          "name": "name2",
-          "val": 2
-        },
-        {
-          "id": "id3",
-          "name": "name3",
-          "val": 5
-        },
-        {
-          "id": "id4",
-          "name": "name4",
-          "val": 10
-        },
-    ],
-    "links": [
-        {
-            "source": "id1",
-            "target": "id2",
-            "distance":300,
-            "width": 1
-
-        },
-        {
-            "source": "id2",
-            "target": "id3",
-            "distance":10,
-            "width": 2
-
-        },
-        {
-            "source": "id2",
-            "target": "id4",
-            "distance":10,
-            "width": 2
-
-        },
-
-    ]
+{
+  "directed": true,
+  "multigraph": false,
+  "graph": {},
+  "nodes": [
+    {
+      "group": "M3",
+      "pseudotime": 2,
+      "val": 9,
+      "id": 0
+    },
+    {
+      "group": "M5",
+      "pseudotime": 5,
+      "val": 7,
+      "id": 1
+    },
+    {
+      "group": "M7",
+      "pseudotime": 5,
+      "val": 7,
+      "id": 2
+    },
+    {
+      "group": "M3",
+      "pseudotime": 1,
+      "val": 4,
+      "id": 3
+    },
+    {
+      "group": "M7",
+      "pseudotime": 6,
+      "val": 5,
+      "id": 4
+    },
+    {
+      "group": "M6",
+      "pseudotime": 6,
+      "val": 5,
+      "id": 5
+    },
+    {
+      "group": "M1",
+      "pseudotime": 0,
+      "val": 3,
+      "id": 6
+    },
+    {
+      "group": "M2",
+      "pseudotime": 7,
+      "val": 4,
+      "id": 7
+    },
+    {
+      "group": "M5",
+      "pseudotime": 6,
+      "val": 4,
+      "id": 8
+    },
+    {
+      "group": "M4",
+      "pseudotime": 7,
+      "val": 4,
+      "id": 9
+    },
+    {
+      "group": "M5",
+      "pseudotime": 4,
+      "val": 4,
+      "id": 10
+    },
+    {
+      "group": "M8",
+      "pseudotime": 7,
+      "val": 2,
+      "id": 11
+    },
+    {
+      "group": "M3",
+      "pseudotime": 4,
+      "val": 3,
+      "id": 12
+    }
+  ],
+  "links": [
+    {
+      "weight": 8.0,
+      "val": 6.0,
+      "distance": 25.0,
+      "source": 0,
+      "target": 12.0
+    },
+    {
+      "weight": 2.0,
+      "val": 6.5,
+      "distance": 100.0,
+      "source": 0,
+      "target": 10.0
+    },
+    {
+      "weight": 5.5,
+      "val": 5.5,
+      "distance": 36.36363636,
+      "source": 1,
+      "target": 8.0
+    },
+    {
+      "weight": 2.0,
+      "val": 6.0,
+      "distance": 100.0,
+      "source": 1,
+      "target": 5.0
+    },
+    {
+      "weight": 8.0,
+      "val": 6.0,
+      "distance": 25.0,
+      "source": 2,
+      "target": 4.0
+    },
+    {
+      "weight": 3.0,
+      "val": 6.5,
+      "distance": 66.66666667,
+      "source": 3,
+      "target": 0.0
+    },
+    {
+      "weight": 2.0,
+      "val": 3.5,
+      "distance": 100.0,
+      "source": 4,
+      "target": 11.0
+    },
+    {
+      "weight": 2.0,
+      "val": 4.5,
+      "distance": 100.0,
+      "source": 4,
+      "target": 7.0
+    },
+    {
+      "weight": 3.0,
+      "val": 3.5,
+      "distance": 66.66666667,
+      "source": 6,
+      "target": 3.0
+    },
+    {
+      "weight": 5.5,
+      "val": 4.0,
+      "distance": 36.36363636,
+      "source": 8,
+      "target": 9.0
+    },
+    {
+      "weight": 5.5,
+      "val": 5.5,
+      "distance": 36.36363636,
+      "source": 10,
+      "target": 1.0
+    },
+    {
+      "weight": 3.0,
+      "val": 3.0,
+      "distance": 66.66666667,
+      "source": 11,
+      "target": 7.0
+    },
+    {
+      "weight": 3.0,
+      "val": 5.0,
+      "distance": 66.66666667,
+      "source": 12,
+      "target": 2.0
+    }
+  ]
 }

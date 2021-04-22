472c7d2740f43cc5775905fc151f20ec2c35d981
Shobi Stassen <35950152+ShobiStassen@users.noreply.github.com>
2021-04-22 21:11:38 +0800

    Update bifurc.js

--- Examples/bifurc.js
+++ Examples/bifurc.js
@@ -1,185 +1,186 @@
+{
 "nodes": [
     {
-      "group": "M3c11",
-      "pseudotime": 2.897804789039272,
+      "group": "M3",
+      "pseudotime": 2.868316558477018,
       "val": 11.0,
       "group_num": 0,
       "id": 0
     },
     {
-      "group": "M6c5",
-      "pseudotime": 6.284654216800585,
+      "group": "M5",
+      "pseudotime": 6.055943098673462,
       "val": 9.157894736842104,
       "group_num": 1,
       "id": 1
     },
     {
-      "group": "M5c1",
-      "pseudotime": 6.074860069502011,
+      "group": "M7",
+      "pseudotime": 6.352083476558033,
       "val": 8.456140350877192,
       "group_num": 2,
       "id": 2
     },
     {
-      "group": "M3c12",
-      "pseudotime": 7.273995873171552,
+      "group": "M7",
+      "pseudotime": 7.660949667064022,
       "val": 6.526315789473684,
-      "group_num": 3,
+      "group_num": 2,
       "id": 3
     },
     {
-      "group": "M7c2",
-      "pseudotime": 1.1564377366517233,
+      "group": "M3",
+      "pseudotime": 1.1446697587885275,
       "val": 6.2631578947368425,
-      "group_num": 4,
+      "group_num": 0,
       "id": 4
     },
     {
-      "group": "M6c5",
-      "pseudotime": 8.918341640861476,
+      "group": "M6",
+      "pseudotime": 7.621164235424791,
       "val": 5.649122807017544,
-      "group_num": 5,
+      "group_num": 3,
       "id": 5
     },
     {
-      "group": "M7c2",
+      "group": "M1",
       "pseudotime": 0.0,
       "val": 4.5964912280701755,
-      "group_num": 6,
+      "group_num": 4,
       "id": 6
     },
     {
-      "group": "M7c2",
-      "pseudotime": 10.0,
+      "group": "M2",
+      "pseudotime": 9.781196049342554,
       "val": 3.982456140350877,
-      "group_num": 7,
+      "group_num": 5,
       "id": 7
     },
     {
-      "group": "M8c10",
-      "pseudotime": 8.35546103621221,
+      "group": "M5",
+      "pseudotime": 7.718093745920888,
       "val": 2.9298245614035086,
-      "group_num": 8,
+      "group_num": 1,
       "id": 8
     },
     {
-      "group": "M5c1",
-      "pseudotime": 9.701780483536746,
+      "group": "M4",
+      "pseudotime": 9.015874830980415,
       "val": 2.9298245614035086,
-      "group_num": 9,
+      "group_num": 6,
       "id": 9
     },
     {
-      "group": "M7c2",
-      "pseudotime": 9.95802407465031,
+      "group": "M8",
+      "pseudotime": 10.0,
       "val": 2.2280701754385968,
-      "group_num": 10,
+      "group_num": 7,
       "id": 10
     },
     {
-      "group": "M6c5",
-      "pseudotime": 4.714268202749234,
+      "group": "M3",
+      "pseudotime": 4.634806799998606,
       "val": 1.7894736842105263,
-      "group_num": 11,
+      "group_num": 0,
       "id": 11
     },
     {
-      "group": "M6c5",
-      "pseudotime": 4.62016785051726,
+      "group": "M3",
+      "pseudotime": 4.628591062417528,
       "val": 1.0,
-      "group_num": 12,
+      "group_num": 0,
       "id": 12
     }
   ],
   "links": [
     {
-      "weight": 5.781062299162181,
+      "weight": 5.781062299162597,
       "val": 6.0,
-      "distance": 34.595717819713684,
+      "distance": 25.94678836478339,
       "source": 0,
       "target": 11
     },
     {
       "weight": 11.0,
       "val": 6.0,
-      "distance": 18.181818181818183,
+      "distance": 13.636363636363637,
       "source": 0,
       "target": 12
     },
     {
       "weight": 1.0,
       "val": 5.078947368421052,
-      "distance": 100.0,
+      "distance": 120.0,
       "source": 1,
       "target": 5
     },
     {
-      "weight": 5.4121880697564775,
+      "weight": 5.412188069756704,
       "val": 5.078947368421052,
-      "distance": 36.9536308462021,
+      "distance": 27.71522313465042,
       "source": 1,
       "target": 8
     },
     {
-      "weight": 10.999999998658577,
+      "weight": 10.999999999699263,
       "val": 4.728070175438596,
-      "distance": 18.18181818403541,
+      "distance": 13.636363636736451,
       "source": 2,
       "target": 3
     },
     {
-      "weight": 2.5837981381242103,
+      "weight": 5.8379813812425425,
       "val": 3.763157894736842,
-      "distance": 50.2584168977681,
+      "distance": 75.69381267332414,
       "source": 3,
       "target": 7
     },
     {
-      "weight": 2.1221200918532572,
+      "weight": 1.122120091854093,
       "val": 3.763157894736842,
-      "distance": 78.2340423739196,
+      "distance": 133.67553178034012,
       "source": 3,
       "target": 10
     },
     {
-      "weight": 5.341306914189697,
+      "weight": 5.341306914190126,
       "val": 3.6315789473684212,
-      "distance": 37.444019453119374,
+      "distance": 28.083014589837273,
       "source": 4,
       "target": 0
     },
     {
-      "weight": 6.000662741005624,
+      "weight": 6.000662740993384,
       "val": 2.7982456140350878,
-      "distance": 33.329651845503136,
+      "distance": 24.99723888417834,
       "source": 6,
       "target": 4
     },
     {
-      "weight": 4.896381751262607,
+      "weight": 4.903288414306135,
+      "val": 2.4912280701754383,
+      "distance": 45.591714646511676,
+      "source": 7,
+      "target": 10
+    },
+    {
+      "weight": 4.8963817510861345,
       "val": 1.9649122807017543,
-      "distance": 40.84648831730225,
+      "distance": 30.63486623908081,
       "source": 8,
       "target": 9
     },
     {
-      "weight": 1.8153322501972315,
-      "val": 1.6140350877192984,
-      "distance": 52.420074290950964,
-      "source": 10,
-      "target": 7
-    },
-    {
-      "weight": 3.368644393184585,
+      "weight": 3.368644393167398,
       "val": 1.3947368421052633,
-      "distance": 59.371063447551315,
+      "distance": 44.52829758589068,
       "source": 11,
       "target": 1
     },
     {
-      "weight": 3.735710423545436,
+      "weight": 3.7357104235510876,
       "val": 1.0,
-      "distance": 53.53734024442579,
+      "distance": 40.1530051832586,
       "source": 12,
       "target": 2
     }

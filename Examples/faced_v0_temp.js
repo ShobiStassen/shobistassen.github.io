f4f05ed4642045c96b598fcceffbe314089a88d0
Shobi Stassen <35950152+ShobiStassen@users.noreply.github.com>
2021-04-28 16:21:39 +0800

    Add files via upload

---
+++ Examples/faced_v0_temp.js
@@ -0,0 +1,246 @@
+var gData={
+  "directed": true,
+  "multigraph": false,
+  "graph": {},
+  "nodes": [
+    {
+      "group": "T2_M2",
+      "pseudotime": 5.351792071146427,
+      "val": 11.0,
+      "group_num": 0,
+      "id": 0
+      "img":'cat.png'
+    },
+    {
+      "group": "T2_M1",
+      "pseudotime": 2.6335133746659514,
+      "val": 10.151943462897526,
+      "group_num": 1,
+      "id": 1
+      "img":'cat.png'
+    },
+    {
+      "group": "T2_M1",
+      "pseudotime": 3.942842679867115,
+      "val": 8.703180212014134,
+      "group_num": 1,
+      "id": 2
+      "img":'cat.png'
+    },
+    {
+      "group": "T2_M1",
+      "pseudotime": 3.461316624868184,
+      "val": 7.431095406360424,
+      "group_num": 1,
+      "id": 3
+      "img":'cat.png'
+    },
+    {
+      "group": "T2_M3",
+      "pseudotime": 9.896175918946204,
+      "val": 7.431095406360424,
+      "group_num": 2,
+      "id": 4
+      "img":'cat.png'
+    },
+    {
+      "group": "T2_M1",
+      "pseudotime": 0.7173543599091575,
+      "val": 7.289752650176679,
+      "group_num": 1,
+      "id": 5
+      "img":'cat.png'
+    },
+    {
+      "group": "T2_M3",
+      "pseudotime": 9.479990251342373,
+      "val": 6.123674911660777,
+      "group_num": 2,
+      "id": 6
+      "img":'cat.png'
+    },
+    {
+      "group": "T2_M1",
+      "pseudotime": 1.8492665407592832,
+      "val": 6.017667844522968,
+      "group_num": 1,
+      "id": 7
+      "img":'cat.png'
+    },
+    {
+      "group": "T2_M1",
+      "pseudotime": 0.0,
+      "val": 5.028268551236749,
+      "group_num": 1,
+      "id": 8
+      "img":'cat.png'
+    },
+    {
+      "group": "T2_M3",
+      "pseudotime": 7.3149339313546555,
+      "val": 4.816254416961131,
+      "group_num": 2,
+      "id": 9
+      "img":'cat.png'
+    },
+    {
+      "group": "T2_M3",
+      "pseudotime": 6.95015438562788,
+      "val": 2.1307420494699647,
+      "group_num": 2,
+      "id": 10
+      "img":'cat.png'
+    },
+    {
+      "group": "T2_M3",
+      "pseudotime": 10.0,
+      "val": 1.353356890459364,
+      "group_num": 2,
+      "id": 11
+      "img":'cat.png'
+    },
+    {
+      "group": "T2_M2",
+      "pseudotime": 8.335251556376418,
+      "val": 1.0,
+      "group_num": 0,
+      "id": 12
+      "img":'cat.png'
+    }
+  ],
+  "links": [
+    {
+      "weight": 4.576936867671126,
+      "val": 6.0,
+      "distance": 32.77301049518828,
+      "source": 0,
+      "target": 9
+    },
+    {
+      "weight": 11.0,
+      "val": 6.0,
+      "distance": 13.636363636363637,
+      "source": 0,
+      "target": 10
+    },
+    {
+      "weight": 4.8301342978865955,
+      "val": 5.575971731448763,
+      "distance": 31.055037137503994,
+      "source": 1,
+      "target": 2
+    },
+    {
+      "weight": 5.929680602908079,
+      "val": 5.575971731448763,
+      "distance": 25.29647211123578,
+      "source": 1,
+      "target": 3
+    },
+    {
+      "weight": 3.925100972390302,
+      "val": 4.851590106007067,
+      "distance": 38.21557739663783,
+      "source": 2,
+      "target": 0
+    },
+    {
+      "weight": 3.314884944851814,
+      "val": 4.215547703180212,
+      "distance": 45.25043930497729,
+      "source": 3,
+      "target": 0
+    },
+    {
+      "weight": 10.881655565820973,
+      "val": 4.215547703180212,
+      "distance": 13.784667148549207,
+      "source": 3,
+      "target": 2
+    },
+    {
+      "weight": 1.000000000484766,
+      "val": 4.144876325088339,
+      "distance": 149.9999999272851,
+      "source": 5,
+      "target": 1
+    },
+    {
+      "weight": 10.999999995898321,
+      "val": 4.144876325088339,
+      "distance": 13.636363641448362,
+      "source": 5,
+      "target": 7
+    },
+    {
+      "weight": 4.803435303107664,
+      "val": 3.5618374558303887,
+      "distance": 31.227650740492948,
+      "source": 6,
+      "target": 4
+    },
+    {
+      "weight": 2.416332552321248,
+      "val": 3.5618374558303887,
+      "distance": 62.0775479997166,
+      "source": 6,
+      "target": 11
+    },
+    {
+      "weight": 7.996949113041722,
+      "val": 3.508833922261484,
+      "distance": 18.757153244275923,
+      "source": 7,
+      "target": 1
+    },
+    {
+      "weight": 1.0405263627045356,
+      "val": 3.508833922261484,
+      "distance": 144.15780837125556,
+      "source": 7,
+      "target": 3
+    },
+    {
+      "weight": 7.587053033949506,
+      "val": 3.0141342756183747,
+      "distance": 19.77052214197008,
+      "source": 8,
+      "target": 5
+    },
+    {
+      "weight": 2.363515843958532,
+      "val": 2.9081272084805656,
+      "distance": 63.4647744729194,
+      "source": 9,
+      "target": 4
+    },
+    {
+      "weight": 5.096176672964249,
+      "val": 2.9081272084805656,
+      "distance": 29.433830423455632,
+      "source": 9,
+      "target": 12
+    },
+    {
+      "weight": 7.4365251549183915,
+      "val": 1.5653710247349824,
+      "distance": 20.170711034412697,
+      "source": 10,
+      "target": 9
+    },
+    {
+      "weight": 2.238181640441467,
+      "val": 1.0,
+      "distance": 67.01868931889436,
+      "source": 12,
+      "target": 4
+    },
+    {
+      "weight": 1.0,
+      "val": 1.0,
+      "distance": 150.0,
+      "source": 12,
+      "target": 6
+    }
+  ]
+}
